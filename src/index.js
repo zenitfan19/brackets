bfec981061c1026d56f1b52c860276f7208d9156
Andrey Lavrov <fanton96mailru@gmail.com>
2020-03-14 23:28:00 +0300

    feat: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let currentStrLength;
+  let regexArr = createRegexes(bracketsConfig);
+  do {
+    currentStrLength = str.length;
+    for (let regexItem of regexArr) {
+      str = str.replace(regexItem, '');      
+    }   
+  } while (str.length !== currentStrLength);
+  return str.length === 0 ? true : false; 
+}
+
+function createRegexes(bracketsConfig) {
+  let regexArr = [];  
+  for (let configItem of bracketsConfig) {
+    let regexStr = '';
+    if(!configItem[0].match(/\d/)) {
+      regexStr += '\\';
+    }
+    regexStr += configItem[0];
+    if(!configItem[1].match(/\d/)) {
+      regexStr += '\\';
+    }
+    regexStr += configItem[1];
+    let regex = new RegExp(regexStr, 'g');    
+    regexArr.push(regex);    
+  }
+  return regexArr;
 }
